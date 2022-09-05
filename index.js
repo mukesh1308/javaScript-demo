28b56e1f0282c2802ec59d7f22ca6f93d58fe49a
mukesh kumar <mukeshkumar130803@gmail.com>
2022-09-05 21:23:20 +0530

    variables and datatype done

---
+++ index.js
@@ -0,0 +1,11 @@
+// inline comment
+/* 
+    this is multiline comment
+*/
+//  variables
+var a="my name is mukesh"
+a="my name is kumar"
+console.log(a);
+let b=5;
+const pi=3.14;
+console.log(b+" "+pi);
