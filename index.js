863d7ea5e0efbde28575cb9a283ba6f408b97d2e
mukesh kumar <mukeshkumar130803@gmail.com>
2022-09-05 22:01:58 +0530

    operators and escape char

--- index.js
+++ index.js
@@ -5,7 +5,54 @@
 //  variables
 var a="my name is mukesh"
 a="my name is kumar"
-console.log(a);
+//! console.log(a);
 let b=5;
 const pi=3.14;
-console.log(b+" "+pi);
+//! console.log(b+" "+pi);
+
+
+// operator
+let add=5+10;
+let sub=100-a;
+let mult=102*3;
+let div=50/5;
+//! console.log("add :"+add);
+//! console.log("sub :"+sub);
+//! console.log("mult :"+mult);
+//! console.log("div :"+div);
+
+let inc=10;
+let dec=20;
+//! console.log("before change :"+inc+" ,"+dec);
+inc++;
+dec--;
+//! console.log("after change :"+inc+" ,"+dec);
+
+
+let remainder=20%2;
+//! console.log("reminder :"+remainder);
+
+let num1=5;
+let num2=10;
+let num3=15;
+let num4=100
+num1+=2  //num1=num1+2
+num2-=5  //num2=num2+5
+num3*=2  //num3=num3*2
+num4/=10 //num4=num4/10
+//! console.log(num1)
+//! console.log(num2)
+//! console.log(num3)
+//! console.log(num4)
+
+/* escap char
+\'
+\"
+\\
+\n
+\t
+\r
+\b
+\f
+*/
+ console.log("my name is mukesh kumar\n i like c++ \t i study ECE#\b ");
